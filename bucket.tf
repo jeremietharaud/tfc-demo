1c0beed4d9873ab2390ad4aeba3bdac8649fbfa2
Jérémie THARAUD <jeremie.tharaud@gmail.com>
2020-12-06 23:00:47 +0100

    Skip some checkov tests

--- bucket.tf
+++ bucket.tf
@@ -1,4 +1,14 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "tfc-demo-${var.region}-664685189390"
   acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
